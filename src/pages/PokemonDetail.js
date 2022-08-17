d3ef969e3978c6f238b0fe4b21752001f9da87ec
Gustavo <ps.gustavo19@gmail.com>
2022-08-17 17:51:37 -0300

    application deployment divided into pages

--- src/pages/PokemonDetail.js
+++ src/pages/PokemonDetail.js
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import Navbar from "../components/Navbar";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import PokemonDetailed from "../components/PokemonDetailed";
 
-function PokemonDetail() {}
+function PokemonDetail() {
+  const [pokemons, setPokemons] = useState({});
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+
+  const searchPokemon = async (id) => {
+    try {
+      setLoading(false);
+      let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+      const response = await fetch(url);
+      const data = await response.json();
+
+      console.log(data);
+      setPokemons(data);
+      setLoading(true);
+    } catch (err) {}
+  };
+
+  useEffect(() => {
+    searchPokemon(id);
+  }, []);
+
+  {
+    console.log(pokemons);
+  }
+
+  return (
+    <div>
+      <Navbar />
+      <PokemonDetailed pokemons={pokemons} />
+    </div>
+  );
+}
 
 export default PokemonDetail;
